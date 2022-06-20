f04b3ab240f1d93086374e1b5a317cfc9b630df3
pranjaladhi <pranjal.adhikari@gmail.com>
2022-06-19 23:56:47 -0500

    updated About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,32 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
+import "../assets/PranjalPic.jpg"
+import PranjalPic from "../assets/PranjalPic.jpg";
 
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
-        <div class="split left">
-          <div className="centered">
-            <img
-              className="profile_image"
-              src={profile_pic}
-              alt="Profile Pic"
-            ></img>
+         <div>
+          <div class="split left">
+            <div className="centered">
+              <img 
+                className="profile_image"
+                src = {PranjalPic}
+                alt="Profile Pic"
+                ></img>
+            </div>
           </div>
-        </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Your Name</div>
-            <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
+          <div className="split right">
+            <div className="centered">
+              <div className="name_title">Pranjal Adhikari </div>
+              <div className="brief_description">
+                  I like mangos and playing tennis
+              </div>
             </div>
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
